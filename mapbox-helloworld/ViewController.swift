9c63b839d4211c9b99aa74881f995e8b72b355b3
lucas <lucas@navisens.com>
2018-09-05 15:30:32 -0700

    Added heading to mapbox navisens integration

--- mapbox-helloworld/ViewController.swift
+++ mapbox-helloworld/ViewController.swift
@@ -31,6 +31,40 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     }
 }
 
+// MotionDnaHeading override class.
+class MotionDnaHeading : CLHeading
+{
+    var motionDnaTrueHeading:CLLocationDirection!
+    var motionDnaMagneticHeading:CLLocationDirection!
+    var motionDnaTimestamp:Date!
+    var motionDnaHeadingAccuracy:CLLocationDirection!
+    override var trueHeading: CLLocationDirection{
+        return self.motionDnaTrueHeading!
+    }
+    override var magneticHeading: CLLocationDirection{
+        return self.motionDnaMagneticHeading!
+    }
+    override var timestamp: Date{
+        return self.motionDnaTimestamp!
+    }
+    override var headingAccuracy: CLLocationDirection{
+        return self.motionDnaHeadingAccuracy!
+    }
+    
+    init(_motionDna: MotionDna!)
+    {
+        super.init()
+        self.motionDnaTrueHeading=_motionDna.getLocation().heading
+        self.motionDnaMagneticHeading=_motionDna.getLocation().magneticHeading
+        self.motionDnaTimestamp=Date(timeIntervalSinceNow: _motionDna.getTimestamp())
+        self.motionDnaHeadingAccuracy=CLLocationDirection(10)// We do not provided a heading accuracy in our SDK yet.
+    }
+    required init(coder: NSCoder)
+    {
+        super.init()
+    }
+}
+
 class MotionDnaLocationManager : NSObject, MGLLocationManager{
     
     override func `self`() -> Self {
@@ -63,11 +97,13 @@ class MotionDnaLocationManager : NSObject, MGLLocationManager{
         var currentLocation = CLLocation(coordinate: CLLocationCoordinate2D(latitude: motionDna.getLocation().globalLocation.latitude, longitude: motionDna.getLocation().globalLocation.longitude), altitude: CLLocationDistance(motionDna.getLocation().absoluteAltitude), horizontalAccuracy: CLLocationAccuracy(motionDna.getLocation().uncertainty.x), verticalAccuracy: CLLocationAccuracy(motionDna.getLocation().absoluteAltitudeUncertainty), course: CLLocationDirection(motionDna.getLocation().heading), speed: CLLocationDistance(motionDna.getMotion().stepFrequency),
                                          timestamp: date)
         var positions: [CLLocation] = []
+        var heading = MotionDnaHeading(_motionDna: motionDna)
+        
         positions.append(currentLocation)
         DispatchQueue.main.async{
-            
             // Need to find a way to assign a CLHeading type from our internal magneticHeading/heading types from MotionDna.getLocation()
             self.delegate?.locationManager(self, didUpdate: positions)
+            self.delegate?.locationManager(self, didUpdate: heading)
         }
     }
     
@@ -92,7 +128,8 @@ class MotionDnaLocationManager : NSObject, MGLLocationManager{
     
     func startUpdatingLocation() {
         controller.start(self)
-        controller.setLocationNavisens()// Configures SDK to use our complete positioning
+        controller.setLocationNavisens()// Configures SDK to use our complete positioning.
+//        controller.setLocationLatitude(37.787742, longitude: -122.396859, andHeadingInDegrees: 315)// Configures SDK to start on inertial estimate from a specific lat/lon/heading.
     }
     
     func stopUpdatingLocation() {
